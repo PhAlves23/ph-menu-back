c3ef9a6cc204371ec90beca6e44fd7f3d864b030
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2023-04-07 17:15:04 -0300

    :tada: start project

---
+++ src/test/java/com/phalves/restaurantmenu/RestaurantMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phalves.restaurantmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
